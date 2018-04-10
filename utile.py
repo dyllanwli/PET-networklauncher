953704818a93b368d9ed2b61b7f2fc596bc07556
dyllanwli <lidiyaa03@gmail.com>
2018-04-10 17:14:39 +0800

    add multiple host kafka case

--- utile.py
+++ utile.py
@@ -1,3 +1,24 @@
 from __future__ import print_function
 import sys
 import os
+
+def replace_ca(fileName,marks,token):
+    result = []
+    with open("./{}".format(fileName),"r") as file:
+        for line in file.readlines():
+            # print(line)
+            for mark in marks:
+                if line.find(mark) > -1:
+                    token_index = line.rfind("/") + 1
+                    line = line[:token_index] + token + "\n"
+                    print(line)
+            result.append(line)
+    with open("./{}".format(fileName),"w") as file:
+        for i in result:
+            file.write(i)
+
+
+fileName = "./docker-compose.yml"
+token = "0000"
+marks = ["FABRIC_CA_SERVER_TLS_KEYFILE","--ca.certfile"]
+replace_ca(fileName,marks,token)
